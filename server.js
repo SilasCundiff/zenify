57e367c3aba704ee4f1a820950e51e2bc6e44473
Silas Cundiff <59065211+SilasCundiff@users.noreply.github.com>
2021-01-04 15:09:58 -0500

    testing

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.get('/callback', function (req, res) {
 
         // we can also pass the token to the browser to make requests from there
         res.redirect(
-          'https://zenify-server.herokuapp.com/' +
+          'https://zenify-server.herokuapp.com/test' +
             querystring.stringify({
               access_token: access_token,
               refresh_token: refresh_token,
