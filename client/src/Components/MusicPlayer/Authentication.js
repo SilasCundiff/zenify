212d89c3cdb5eb48728a956efd0e688c381e4252
Silas Cundiff <59065211+SilasCundiff@users.noreply.github.com>
2021-01-04 16:22:19 -0500

    so close

--- client/src/Components/MusicPlayer/Authentication.js
+++ client/src/Components/MusicPlayer/Authentication.js
@@ -5,15 +5,39 @@ import setToken from '../../reducers/auth';
 import logo from '../logo.svg';
 import './authenticationStyles.css';
 function Authentication({ getToken }) {
-  // useEffect(() => {
+  useEffect(() => {
+    /**
+     * Obtains parameters from the hash of the URL
+     * @return Object
+     */
+    const getHashParams = () => {
+      var hashParams = {};
+      var e,
+        r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+      while ((e = r.exec(q))) {
+        hashParams[e[1]] = decodeURIComponent(e[2]);
+      }
+      let hash = hashParams.access_token;
+      if (hash) {
+        getToken();
+      }
+    };
+    getHashParams();
+  }, [getToken]);
 
-  //   getHashParams();
-  // }, [getToken]);
+  let success = false;
+  const handleLogin = () => {
+    while (!success) {
+      getHashParams();
+    }
+  };
 
   /**
    * Obtains parameters from the hash of the URL
    * @return Object
    */
+
   const getHashParams = () => {
     var hashParams = {};
     var e,
@@ -25,6 +49,7 @@ function Authentication({ getToken }) {
     let hash = hashParams.access_token;
     if (hash) {
       getToken();
+      success = true;
     }
   };
   return (
@@ -35,7 +60,7 @@ function Authentication({ getToken }) {
         <span className='introName'> By Silvanus Designs </span>
       </div>
       <img src={logo} alt='Silvanus Designs' className='introLogo' />
-      <a href='/login' onClick={getHashParams}>
+      <a href='/login' onClick={handleLogin}>
         <button className='loginButton'>Spotify Login Required</button>
       </a>
     </div>
