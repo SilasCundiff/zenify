507ddd033d56c774cf137a8fa41346f112aafc5c
Silas Cundiff <59065211+SilasCundiff@users.noreply.github.com>
2021-01-04 17:15:45 -0500

    test

--- client/src/Components/MusicPlayer/Authentication.js
+++ client/src/Components/MusicPlayer/Authentication.js
@@ -1,20 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getToken } from '../../actions/auth.js';
 import setToken from '../../reducers/auth';
 import logo from '../logo.svg';
 import './authenticationStyles.css';
 function Authentication({ getToken, token, isLoggedIn }) {
-  const [tryLogin, setTryLogin] = useState(false);
-
   useEffect(() => {
-    if (tryLogin) {
-      getToken();
-    }
-  }, [getToken, tryLogin]);
-  const handleClick = () => {
-    setTryLogin(true);
-  };
+    /**
+     * Obtains parameters from the hash of the URL
+     * @return Object
+     */
+    const getHashParams = () => {
+      if (token === undefined && isLoggedIn === false) {
+        var hashParams = {};
+        var e,
+          r = /([^&;=]+)=?([^&;]*)/g,
+          q = window.location.hash.substring(1);
+        while ((e = r.exec(q))) {
+          hashParams[e[1]] = decodeURIComponent(e[2]);
+        }
+        let hash = hashParams.access_token;
+        if (hash) {
+          getToken();
+        }
+      }
+    };
+    getHashParams();
+  }, [getToken, token, isLoggedIn]);
+
   return (
     <div className='authenticationBody'>
       <div className='introTitle'>
@@ -23,7 +36,7 @@ function Authentication({ getToken, token, isLoggedIn }) {
         <span className='introName'> By Silvanus Designs </span>
       </div>
       <img src={logo} alt='Silvanus Designs' className='introLogo' />
-      <a href='/login' onClick={handleClick}>
+      <a href='/login'>
         <button className='loginButton'>Spotify Login Required</button>
       </a>
     </div>
