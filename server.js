3df08f0ce4309b09ccf8e57091ca7f4d93cdc1c4
Silas Cundiff <59065211+SilasCundiff@users.noreply.github.com>
2021-01-04 14:33:46 -0500

    refactor testing

--- server.js
+++ server.js
@@ -33,13 +33,6 @@ var client_secret = process.env.CLIENT_SECRET; // Your secret
 var redirect_uri = process.env.REDIRECT_URI; // Your redirect uri
 const PORT = process.env.PORT || 8080;
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
@@ -178,5 +171,11 @@ app.get('/refresh_token', function (req, res) {
     }
   });
 });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT);
