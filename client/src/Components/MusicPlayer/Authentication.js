01cfdda665401183842565fa2e4d307783b02809
Silas Cundiff <59065211+SilasCundiff@users.noreply.github.com>
2021-01-04 17:45:27 -0500

    test

--- client/src/Components/MusicPlayer/Authentication.js
+++ client/src/Components/MusicPlayer/Authentication.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useLocation, useState } from 'react';
+import React, { useEffect, useLocation } from 'react';
 import { connect } from 'react-redux';
 import { getToken } from '../../actions/auth.js';
 import setToken from '../../reducers/auth';
@@ -6,29 +6,27 @@ import logo from '../logo.svg';
 import './authenticationStyles.css';
 function Authentication({ getToken }) {
   const newLocation = useLocation();
-
+  console.log('newLocation', newLocation);
   useEffect(() => {
-    if (newLocation !== '/') {
-      /**
-       * Obtains parameters from the hash of the URL
-       * @return Object
-       */
-      const getHashParams = () => {
-        var hashParams = {};
-        var e,
-          r = /([^&;=]+)=?([^&;]*)/g,
-          q = window.location.hash.substring(1);
-        while ((e = r.exec(q))) {
-          hashParams[e[1]] = decodeURIComponent(e[2]);
-        }
-        let hash = hashParams.access_token;
-        if (hash) {
-          getToken();
-        }
-      };
-      getHashParams();
-    }
-  }, [getToken, newLocation]);
+    /**
+     * Obtains parameters from the hash of the URL
+     * @return Object
+     */
+    const getHashParams = () => {
+      var hashParams = {};
+      var e,
+        r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+      while ((e = r.exec(q))) {
+        hashParams[e[1]] = decodeURIComponent(e[2]);
+      }
+      let hash = hashParams.access_token;
+      if (hash) {
+        getToken();
+      }
+    };
+    getHashParams();
+  }, [getToken]);
 
   return (
     <div className='authenticationBody'>
