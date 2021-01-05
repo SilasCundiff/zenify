acd258c83889b2aab55bb43ef71d34adc5ae7409
Silas Cundiff <59065211+SilasCundiff@users.noreply.github.com>
2021-01-05 13:15:19 -0500

    fix import of useLocation

--- client/src/Components/MusicPlayer/Authentication.js
+++ client/src/Components/MusicPlayer/Authentication.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useLocation } from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getToken } from '../../actions/auth.js';
 import setToken from '../../reducers/auth';
